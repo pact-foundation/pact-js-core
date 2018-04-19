35da5697dc8c0beffaaf65f1a0942b41ccb6eff4
Michel Boudreau <michelboudreau@gmail.com>
2018-04-19 22:12:17 +1030

    feat(open-collective): begging for money on postinstall with opencollective link (#87)
    * feat(money-beg): begging for money on postinstall with opencollective link
    
    * updating donation url to be donate.pact.io
    
    * updating url to include technology query
    
    * Adding google analytics tracking for download
    
    * updating donate url to use path instead of query, adding check for environment variable to not track CI servers
    
    * adding stats for if it's CI downloading pact or a user, adding message about tracking with ability to disable tracking

--- standalone/install.ts
+++ standalone/install.ts
@@ -14,8 +14,28 @@ function download(data: Data): Promise<Data> {
 			console.log(chalk.yellow("Binary already downloaded, skipping..."));
 			return resolve(data);
 		}
-
 		console.log(chalk.yellow(`Downloading Pact Standalone Binary v${PACT_STANDALONE_VERSION} for platform ${data.platform} from ${data.url}`));
+
+		// Track downloads through Google Analytics unless testing or don't want to be tracked
+		if (!process.env.DO_NOT_TRACK) {
+			console.log(chalk.gray("Please note: we are tracking this download anonymously to gather important usage statistics. " +
+			"To disable tracking, set 'DO_NOT_TRACK=true' as an environment variable."));
+			const CI = ["CI", "CONTINUOUS_INTEGRATION"].some((key) => process.env[key] !== undefined);
+			request.post({
+				url: "https://www.google-analytics.com/collect",
+				form: {
+					v: 1,
+					tid: "UA-117778936-1", // Tracking ID / Property ID.
+					cid: Math.round(2147483647 * Math.random()).toString(), // Anonymous Client ID.
+					t: "screenview", // Screenview hit type.
+					an: "pact-install", // App name.
+					av: require("../package.json").version, // App version.
+					aid: "pact-node", // App Id.
+					aiid: `standalone-${PACT_STANDALONE_VERSION}`, // App Installer Id.
+					cd: `download-node-${data.platform}-${CI ? "ci" : "user"}`
+				}
+			});
+		}
 
 		// Get archive of release
 		let len = 0;
@@ -64,10 +84,22 @@ function extract(data: Data): Promise<void> {
 		.then(() => {
 			// Remove pact-publish as it's getting deprecated
 			const publishPath = path.resolve(data.platformFolderPath, "bin", `pact-publish${data.isWindows ? ".bat" : ""}`);
-			if(fs.existsSync(publishPath)) {
+			if (fs.existsSync(publishPath)) {
 				fs.unlinkSync(publishPath);
 			}
 			console.log(chalk.green("Extraction done."));
+		})
+		.then(() => {
+			console.log(
+				"\n\n" +
+				chalk.bgYellow(
+					chalk.black("### If you") +
+					chalk.red(" ❤ ") +
+					chalk.black("Pact and want to support us, please donate here:")
+				) +
+				chalk.blue(" http://donate.pact.io/node") +
+				"\n\n"
+			);
 		})
 		.catch((e: any) => Promise.reject(`Extraction failed for ${data.filepath}: ${e}`));
 }
