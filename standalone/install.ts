f33cbb8e960e34f7f114821061cff701049beac8
Matt Fellows <matt.fellows@onegeek.com.au>
2020-02-14 11:48:30 +1100

    feat(upgrade): update standalone to 1.79.0

--- standalone/install.ts
+++ standalone/install.ts
@@ -21,7 +21,7 @@ const request = Request.defaults({
 });
 
 // Get latest version from https://github.com/pact-foundation/pact-ruby-standalone/releases
-export const PACT_STANDALONE_VERSION = '1.77.0';
+export const PACT_STANDALONE_VERSION = '1.79.0';
 const PACT_DEFAULT_LOCATION = `https://github.com/pact-foundation/pact-ruby-standalone/releases/download/v${PACT_STANDALONE_VERSION}/`;
 const HTTP_REGEX = /^http(s?):\/\//;
 
