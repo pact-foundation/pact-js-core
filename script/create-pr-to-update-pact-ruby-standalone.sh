132be91561a0f8cbeaee7e51e4c1bc2f1bd6faab
Beth Skurrie <beth@bethesque.com>
2020-04-08 10:42:09 +1000

    chore: default PR for updating standalone to fix

--- script/create-pr-to-update-pact-ruby-standalone.sh
+++ script/create-pr-to-update-pact-ruby-standalone.sh
@@ -3,6 +3,7 @@
 : "${1?Please supply the pact-ruby-standalone version to upgrade to}"
 
 STANDALONE_VERSION=$1
+TYPE=${2:-fix}
 DASHERISED_VERSION=$(echo "${STANDALONE_VERSION}" | sed 's/\./\-/g')
 BRANCH_NAME="chore/upgrade-to-pact-ruby-standalone-${DASHERISED_VERSION}"
 
@@ -16,9 +17,9 @@ cat standalone/install.ts | sed "s/export const PACT_STANDALONE_VERSION.*/export
 mv tmp-install standalone/install.ts
 
 git add standalone/install.ts
-git commit -m "feat(upgrade): update standalone to ${STANDALONE_VERSION}"
+git commit -m "${TYPE}: update standalone to ${STANDALONE_VERSION}"
 git push --set-upstream origin ${BRANCH_NAME}
 
-hub pull-request --browse --message "feat: update standalone to ${STANDALONE_VERSION}"
+hub pull-request --browse --message "#{TYPE}: update standalone to ${STANDALONE_VERSION}"
 
 git checkout master
