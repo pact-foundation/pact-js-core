0f34015cacfaddafe02a0a892a21432bc4838348
Timothy Jones <timothy.l.jones@gmail.com>
2021-01-27 17:04:48 +1100

    chore(ci): Add script header

--- script/ci/build-and-test.sh
+++ script/ci/build-and-test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -eu
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)" # Figure out where the script is running
+. "$SCRIPT_DIR"/lib/robust-bash.sh
 
 "$SCRIPT_DIR"/lib/modify-permissions-github-actions.sh
 
