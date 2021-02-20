b0765b067b354df5e41e9c5f37f3fa69e8d84738
Timothy Jones <timothy.l.jones@gmail.com>
2021-02-20 17:22:59 +1100

    chore(github-actions): Remove workaround for github actions permissions issue

--- script/ci/build-and-test.sh
+++ script/ci/build-and-test.sh
@@ -2,8 +2,6 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)" # Figure out where the script is running
 . "$SCRIPT_DIR"/lib/robust-bash.sh
 
-"$SCRIPT_DIR"/lib/modify-permissions-github-actions.sh
-
 npm ci --ignore-scripts
 npm test
 npm run download-checksums
