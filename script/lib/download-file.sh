bdcc7d230d9660bf91d1937e06aa35167732b9e7
Timothy Jones <timothy.l.jones@gmail.com>
2021-08-25 17:22:28 +1000

    debug: add extra printout during download

--- script/lib/download-file.sh
+++ script/lib/download-file.sh
@@ -11,10 +11,13 @@ function download_to {
     error "${FUNCNAME[0]} requires the file to save the download in as the second argument"
     exit 1
   fi
+  log "about to download"
   URL="$1"
   OUTPUT_FILE="$2"
+  log "doing curl of: '$URL', saving in $OUTPUT_FILE"
 
   HTTP_CODE=$(curl --silent --output "$OUTPUT_FILE" --write-out "%{http_code}" --location "$URL")
+  log "did curl, was ${HTTP_CODE}"
   if [[ "${HTTP_CODE}" -lt 200 || "${HTTP_CODE}" -gt 299 ]] ; then
     error "Unable to download file at url ${URL}"
     error "Downloaded content follows"
