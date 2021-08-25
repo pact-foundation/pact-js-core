b37c570796ab26123c75f7e786034acf239bd77c
Timothy Jones <timothy.l.jones@gmail.com>
2021-08-25 18:00:16 +1000

    chore: move ONLY_DOWNLOAD_PACT_FOR_WINDOWS environment variable to build step instead of sub-script

--- script/ci/build-and-test-windows.sh
+++ script/ci/build-and-test-windows.sh
@@ -2,10 +2,6 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)" # Figure out where the script is running
 . "$SCRIPT_DIR"/../lib/robust-bash.sh
 
-# The windows build agent has trouble unpacking the tar for
-# linux, so we only download windows binaries. This means
-# we cannot release from Windows in CI.
-export ONLY_DOWNLOAD_PACT_FOR_WINDOWS=true
 
 npm ci
 # We don't check format on windows because it always fails
