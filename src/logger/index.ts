c5cf07796a564fd1d70cafcd6d705d8b3e81a3f7
Timothy Jones <timothy.l.jones@gmail.com>
2021-09-06 21:26:34 +1000

    fix: Don't create logger on each log call, so that frameworks that check for imports after teardown (jest) don't throw errors deep inside pino

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -7,25 +7,30 @@ const pkg = require('../../package.json');
 
 const logContext = `pact-core@${pkg.version}`;
 let currentLogLevel: LogLevel = 'info';
+let logger = createLogger(currentLogLevel);
 
 export const setLogLevel = (level: LogLevel = 'info'): void => {
   currentLogLevel = level;
+  logger = createLogger(currentLogLevel);
 };
 
 export const verboseIsImplied = (): boolean =>
   currentLogLevel === 'trace' || currentLogLevel === 'debug';
 
+const addContext = (context: string, message: string) =>
+  `${context}: ${message}`;
+
 export default {
   pactCrash: (message: string, context: string = logContext): void =>
-    createLogger(currentLogLevel, context).error(pactCrashMessage(message)),
+    logger.error(addContext(context, pactCrashMessage(message))),
   error: (message: string, context: string = logContext): void =>
-    createLogger(currentLogLevel, context).error(message),
+    logger.error(addContext(context, message)),
   warn: (message: string, context: string = logContext): void =>
-    createLogger(currentLogLevel, context).warn(message),
+    logger.warn(addContext(context, message)),
   info: (message: string, context: string = logContext): void =>
-    createLogger(currentLogLevel, context).info(message),
+    logger.info(addContext(context, message)),
   debug: (message: string, context: string = logContext): void =>
-    createLogger(currentLogLevel, context).debug(message),
+    logger.debug(addContext(context, message)),
   trace: (message: string, context: string = logContext): void =>
-    createLogger(currentLogLevel, context).trace(message),
+    logger.trace(addContext(context, message)),
 };
--- src/logger/pino.ts
+++ src/logger/pino.ts
@@ -1,11 +1,10 @@
 import pino = require('pino');
 import { LogLevel } from './types';
 
-export const createLogger = (level: LogLevel, context: string): pino.Logger =>
+export const createLogger = (level: LogLevel): pino.Logger =>
   pino({
     level: level.toLowerCase(),
     prettyPrint: {
-      messageFormat: `${context}: {msg}`,
       translateTime: true,
     },
   });
