bb521587d7c411ea78520733c3178dd92945b89b
mefellows <mefellows@users.noreply.github.com>
2022-02-16 18:16:16 +1100

    fix: remove overall verification timeout (see https://github.com/pact-foundation/pact-js/issues/761)

--- src/verifier/index.ts
+++ src/verifier/index.ts
@@ -1,5 +1,4 @@
 import logger from '../logger';
-import { timeout, TimeoutError } from 'promise-timeout';
 
 import { VerifierOptions } from './types';
 import { verify } from './nativeVerifier';
@@ -21,16 +20,7 @@ export class Verifier {
   public verify(): Promise<string> {
     logger.info('Verifying Pact Files');
 
-    return timeout(verify(this.options), this.options.timeout as number).catch(
-      (err: Error) => {
-        if (err instanceof TimeoutError) {
-          throw new Error(
-            `Timeout waiting for verification process to complete`
-          );
-        }
-        throw err;
-      }
-    );
+    return verify(this.options);
   }
 }
 
