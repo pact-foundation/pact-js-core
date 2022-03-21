7407333a0382ae4a6f1d90d427e5f76ea4bbc506
mefellows <mefellows@users.noreply.github.com>
2022-03-21 12:30:38 +1100

    fix: update rust core to 0.2.4

--- src/ffi/index.ts
+++ src/ffi/index.ts
@@ -5,7 +5,7 @@ import bindings = require('bindings');
 
 const ffiLib: Ffi = bindings('pact.node');
 
-export const PACT_FFI_VERSION = '0.2.3';
+export const PACT_FFI_VERSION = '0.2.4';
 
 let ffi: typeof ffiLib;
 let ffiLogLevel: LogLevel;
