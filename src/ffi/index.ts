81483b895fd891ff6cd46b9ab1b42a899cc546d9
Matt Fellows <matt.fellows@onegeek.com.au>
2022-08-30 10:26:15 +1000

    fix: update rust core to 0.3.9

--- src/ffi/index.ts
+++ src/ffi/index.ts
@@ -5,7 +5,7 @@ import bindings = require('bindings');
 
 const ffiLib: Ffi = bindings('pact.node');
 
-export const PACT_FFI_VERSION = '0.3.8';
+export const PACT_FFI_VERSION = '0.3.9';
 
 let ffi: typeof ffiLib;
 let ffiLogLevel: LogLevel;
