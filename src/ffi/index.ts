0da476dbd342f64234515572408fd43b28b5085b
Matt Fellows <matt.fellows@onegeek.com.au>
2022-11-28 17:47:36 +1100

    fix: upgrade to ffi 0.3.15

--- src/ffi/index.ts
+++ src/ffi/index.ts
@@ -5,7 +5,7 @@ import bindings = require('bindings');
 
 const ffiLib: Ffi = bindings('pact.node');
 
-export const PACT_FFI_VERSION = '0.3.14';
+export const PACT_FFI_VERSION = '0.3.15';
 
 let ffi: typeof ffiLib;
 let ffiLogLevel: LogLevel;
