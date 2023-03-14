2bb924d943c2d023708a5f2d6b5b94f004b600b2
Voon Wong <voon.wong@smartbear.com>
2023-03-14 12:09:32 +1100

    chore: fix formatting

--- src/logger/pino.ts
+++ src/logger/pino.ts
@@ -3,6 +3,9 @@ import pretty from 'pino-pretty';
 import { LogLevel } from './types';
 
 export const createLogger = (level: LogLevel): pino.Logger =>
-  pino({
-    level: level.toLowerCase(),
-  }, pretty({ sync: true }));
+  pino(
+    {
+      level: level.toLowerCase(),
+    },
+    pretty({ sync: true })
+  );
