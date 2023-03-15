8e4f735c3628d7155b4451d7254a801b86b19d8a
Timothy Jones <timothy.l.jones@gmail.com>
2023-03-15 16:06:41 +1100

    feat!: Remove the Ruby verifier and the associated CLI stubs (pact-broker, pact-message, pact-mock-service, pact-provider-verifier, pact-stub-service, pact, and pactflow). These have moved to @pact-foundation/pact-cli

--- script/lib/export-binary-versions.sh
+++ script/lib/export-binary-versions.sh
@@ -2,5 +2,4 @@
 LIB_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)" # Figure out where the script is running
 PROJECT_DIR="${LIB_DIR}"/../../
 
-export STANDALONE_VERSION=$(grep "PACT_STANDALONE_VERSION = '" "$PROJECT_DIR"/standalone/install.ts | grep -E -o "'(.*)'" | cut -d"'" -f2)
 export FFI_VERSION=v$(grep "PACT_FFI_VERSION = '" "$PROJECT_DIR"/src/ffi/index.ts | grep -E -o "'(.*)'" | cut -d"'" -f2)
