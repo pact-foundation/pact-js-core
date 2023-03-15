e9f569b5a72279b522a21ad18809279acfe54be1
Timothy Jones <timothy.l.jones@gmail.com>
2023-03-15 16:25:48 +1100

    feat!: Remove all classes that were backed by the Ruby implementation (Publisher; Message; Server; AbstractService; Stub; CanDeploy; CannotDeployError). Also remove the Pact() methods that called them (createServer; listServer; removeAllServers; createStub; listStub; createMessage; publishPacts). If you need these features, please use @pact-foundation/pact-cli

--- src/spawn/index.ts
+++
@@ -1,5 +0,0 @@
-import spawn from './spawn';
-
-export * from './arguments';
-
-export default spawn;
--- src/types.ts
+++ src/types.ts
@@ -1,41 +1,4 @@
 import needle from 'needle';
-import { LogLevel } from './logger/types';
-
-export interface MessageOptions {
-  content?: string;
-  dir?: string;
-  consumer?: string;
-  provider?: string;
-  pactFileWriteMode?: 'overwrite' | 'update' | 'merge';
-  spec?: number;
-}
-
-export interface PublisherOptions {
-  pactFilesOrDirs: string[];
-  pactBroker: string;
-  consumerVersion: string;
-  pactBrokerUsername?: string;
-  pactBrokerPassword?: string;
-  pactBrokerToken?: string;
-  tags?: string[];
-  verbose?: boolean;
-  timeout?: number;
-  buildUrl?: string;
-  branch?: string;
-  autoDetectVersionProperties?: boolean;
-}
-
-export interface ServiceOptions {
-  port?: number;
-  ssl?: boolean;
-  cors?: boolean;
-  host?: string;
-  sslcert?: string;
-  sslkey?: string;
-  log?: string;
-  logLevel?: LogLevel;
-  timeout?: number;
-}
 
 export interface HTTPConfig extends Omit<needle.NeedleOptions, 'headers'> {
   headers: {
