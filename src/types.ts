a52fe415a49c514124347775329f40a27fa114a4
Timothy Jones <timothy.l.jones@gmail.com>
2023-03-15 16:45:23 +1100

    build: Move rimraf to devDependencies; remove a number of now unused dependencies

--- src/types.ts
+++
@@ -1,8 +0,0 @@
-import needle from 'needle';
-
-export interface HTTPConfig extends Omit<needle.NeedleOptions, 'headers'> {
-  headers: {
-    'X-Pact-Mock-Service': string;
-    'Content-Type': string;
-  };
-}
