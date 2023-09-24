6ddfda7d43a1590d9dfcf7171c1afeffd228ac05
Matt Fellows <53900+mefellows@users.noreply.github.com>
2023-09-25 08:43:20 +1000

    fix: upgrade to latest ffi 0.4.9

--- src/ffi/index.ts
+++ src/ffi/index.ts
@@ -4,7 +4,7 @@ import logger, { DEFAULT_LOG_LEVEL } from '../logger';
 import { LogLevel } from '../logger/types';
 import { Ffi } from './types';
 
-export const PACT_FFI_VERSION = '0.4.7';
+export const PACT_FFI_VERSION = '0.4.9';
 
 // supported prebuilds
 // darwin-arm64
