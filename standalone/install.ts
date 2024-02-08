fa93ec8e5bac74ab04d6b0782c3b2aeab21c57ee
pact-dev <pact-dev@users.noreply.github.com>
2024-02-08 14:27:28 +0000

    fix: update standalone to 2.2.0

--- standalone/install.ts
+++ standalone/install.ts
@@ -1,7 +1,7 @@
 import chalk = require('chalk');
 
 // Get latest version from https://github.com/pact-foundation/pact-ruby-standalone/releases
-export const PACT_STANDALONE_VERSION = '2.1.0';
+export const PACT_STANDALONE_VERSION = '2.2.0';
 
 function makeError(msg: string): Error {
   return new Error(chalk.red(`Error while locating pact binary: ${msg}`));
