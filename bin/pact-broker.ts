2f92868a380e439b4f95590bda0da10c054da999
Tim Vahlbrock <tim@vahlbrock.de>
2024-05-06 15:01:18 +0200

    fix: node doesn't run .bat files on windows
    Signed-off-by: Tim Vahlbrock <tim@vahlbrock.de>

--- bin/pact-broker.ts
+++ bin/pact-broker.ts
@@ -3,11 +3,13 @@
 import childProcess = require('child_process');
 import rubyStandalone from '../src/pact-standalone';
 
-const { status } = childProcess.spawnSync(
+const { error, status } = childProcess.spawnSync(
   rubyStandalone.brokerFullPath,
   process.argv.slice(2),
   {
     stdio: 'inherit',
+    shell: true,
   }
 );
+if (error) throw error;
 process.exit(status as number);
