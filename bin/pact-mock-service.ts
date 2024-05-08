4f125b32bc3bdfc5e9bfbbef4e0302073783dcf1
Tim Vahlbrock <tim@vahlbrock.de>
2024-05-08 09:11:52 +0200

    fix: node doesn't run .bat files on windows (#504)
    Signed-off-by: Tim Vahlbrock <tim@vahlbrock.de>

--- bin/pact-mock-service.ts
+++ bin/pact-mock-service.ts
@@ -3,9 +3,13 @@
 import childProcess = require('child_process');
 import rubyStandalone from '../src/pact-standalone';
 
-const { status } = childProcess.spawnSync(
+const { error, status } = childProcess.spawnSync(
   rubyStandalone.mockServiceFullPath,
   process.argv.slice(2),
-  { stdio: 'inherit' }
+  {
+    stdio: 'inherit',
+    shell: true,
+  }
 );
+if (error) throw error;
 process.exit(status as number);
