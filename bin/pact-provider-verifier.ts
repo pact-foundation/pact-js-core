d5ec9ea387faa131e0b82fa247dfc33ab4b26dd7
Yousaf Nabi <yousaf@pactflow.io>
2024-05-28 17:53:23 +0100

    fix: shell escaping on non windows platforms

--- bin/pact-provider-verifier.ts
+++ bin/pact-provider-verifier.ts
@@ -3,12 +3,14 @@
 import childProcess = require('child_process');
 import rubyStandalone from '../src/pact-standalone';
 
+const isWindows = process.platform === 'win32'
+const opts = isWindows ? { shell: true } : {}
 const { error, status } = childProcess.spawnSync(
   rubyStandalone.verifierFullPath,
   process.argv.slice(2),
   {
     stdio: 'inherit',
-    shell: true,
+    ...opts
   }
 );
 if (error) throw error;
