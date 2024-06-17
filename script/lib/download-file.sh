60961cc636d21e87e3933796609d7479e7c5a449
Yousaf Nabi <you@saf.dev>
2024-06-17 16:37:42 +0100

    ci(win): workaround for broken curl 8.8.0 --write-out opt

--- script/lib/download-file.sh
+++ script/lib/download-file.sh
@@ -16,7 +16,19 @@ function download_to {
   OUTPUT_FILE="$2"
   debug_log "doing curl of: '$URL', saving in $OUTPUT_FILE"
 
-  HTTP_CODE="$(curl --silent --output "$OUTPUT_FILE" --write-out "%{http_code}" --location "$URL")"
+  if [[ "$(uname -m)" == "Darwin" ]] || [[ "$(uname -m)" == "Linux" ]]; then
+    HTTP_CODE="$(curl --silent --output "$OUTPUT_FILE" --write-out "%{http_code}" --location "$URL")"
+  else
+  # temp workaround for curl 8.8.x error on windows gha runners
+  # https://github.com/curl/curl/issues/13845
+    curl --silent --output "$OUTPUT_FILE" --location "$URL"
+    if [ $? -ne 0 ]; then
+      error "Unable to download file at url ${URL}"
+      exit 1
+    else
+      HTTP_CODE=200
+    fi
+  fi
   debug_log "did curl, http code was '${HTTP_CODE}'"
   if [[ "${HTTP_CODE}" -lt 200 || "${HTTP_CODE}" -gt 299 ]] ; then
     error "Unable to download file at url ${URL}"
