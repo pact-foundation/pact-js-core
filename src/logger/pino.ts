1a6df7a28d8c9122b304cd1e789933191b133bc7
Matt Fellows <53900+mefellows@users.noreply.github.com>
2025-10-17 22:28:43 +1100

    fix: prevent missing pino logs
    This change overwrites the default pino-pretty behaviour
    by writing to console.log with the (pretty) formatted
    message.
    
    Fixes https://github.com/pact-foundation/pact-js/issues/1113

--- src/logger/pino.ts
+++ src/logger/pino.ts
@@ -1,11 +1,21 @@
 import { pino, Logger } from 'pino';
-import pretty from 'pino-pretty';
+import { prettyFactory } from 'pino-pretty';
+
 import { LogLevel } from './types';
 
-export const createLogger = (level: LogLevel): Logger =>
-  pino(
+export const createLogger = (level: LogLevel): Logger => {
+  const prettify = prettyFactory({ sync: true });
+  // Configure custom stream that forwards pretty message to console
+  // to address issues with pino pretty and Jest
+  // Thanks to https://github.com/pinojs/pino-pretty/issues/480#issuecomment-2271709217 for the solution
+  return pino(
     {
       level: level.toLowerCase(),
     },
-    pretty({ sync: true })
+    {
+      write(data: unknown) {
+        console.log(prettify(data));
+      },
+    }
   );
+};
